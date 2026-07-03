9b6453e8297b7d3ee010c900db4c3ff80e2f04ab
shalonjovan <shalonjovan@gmail.com>
2026-07-04 00:35:46 +0530

    add Url JPA entity and repository

---
+++ URL-SHORTENER/src/main/java/com/urlshortener/model/Url.java
@@ -0,0 +1,39 @@
+package com.urlshortener.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "short_code", nullable = false, unique = true, length = 8)
+    private String shortCode;
+
+    @Column(name = "long_url", nullable = false, columnDefinition = "TEXT")
+    private String longUrl;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
